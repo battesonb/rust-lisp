8e6b2770f49fb2c66aec1e14ef8968af9fc3db39
Byron Batteson <byronbatteson@gmail.com>
2026-01-10 09:47:24 +0000

    Add basic rustlisp-wasm project
    This just serves to create a static web page to demonstrate the
    interpreter. Currently, print seems to just go to /dev/null. The
    interpreter should expose configuration for setting the stdout and
    stderr callbacks.

---
+++ rustlisp-wasm/README.md
@@ -0,0 +1,13 @@
+# rustlisp-wasm
+
+## Building
+
+```sh
+wasm-pack build --target web
+```
+
+Then, run a static server in the crate root:
+
+```sh
+python3 -m http.server
+```
---
+++ rustlisp-wasm/src/lib.rs
@@ -0,0 +1,84 @@
+use rustlisp::{interpreter::Interpreter, lexer::Lexer, parser::Parser};
+use wasm_bindgen::prelude::*;
+
+const EXAMPLE: &'static str = r#"(defun fact (n)
+    (if (<= n 1)
+        1
+        (* n (fact (- n 1)))))
+(print (fact 5))"#;
+
+#[wasm_bindgen(start)]
+fn main() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+
+    let input = document
+        .get_element_by_id("input")
+        .expect("Expected an input")
+        .dyn_into::<web_sys::HtmlTextAreaElement>()
+        .expect("Input must be a text area");
+    let submit = document
+        .get_element_by_id("submit")
+        .expect("Expected an submit");
+    let output = document
+        .get_element_by_id("output")
+        .expect("Expected an output")
+        .dyn_into::<web_sys::HtmlElement>()
+        .expect("Output must be an HTML element");
+
+    input.set_text_content(EXAMPLE.into());
+
+    let set_output = {
+        let output = output.clone();
+        move |text: String, err: bool| {
+            output.set_text_content(Some(&text));
+            output
+                .style()
+                .set_property("color", if err { "white" } else { "" })
+                .expect("Failed to set output CSS color");
+            output
+                .style()
+                .set_property("background-color", if err { "#f04f43" } else { "" })
+                .expect("Failed to set output CSS background-color");
+        }
+    };
+
+    let mut interpreter = Interpreter::new();
+    interpreter.load_std();
+
+    let submit_callback = Closure::<dyn FnMut(_)>::new(move |_: web_sys::MouseEvent| {
+        let text = input.value();
+        let lexer = Lexer::new(&text);
+        let tokens = match lexer.lex() {
+            Ok(s) => s,
+            Err(e) => {
+                set_output(e.to_string(), true);
+                return;
+            }
+        };
+        let parser = Parser::new(tokens);
+        let statements = match parser.parse() {
+            Ok(s) => s,
+            Err(e) => {
+                set_output(e.to_string(), true);
+                return;
+            }
+        };
+        for value in statements {
+            match interpreter.evaluate(value) {
+                Ok(result) => {
+                    set_output(result.to_string(), false);
+                }
+                Err(e) => {
+                    set_output(e.to_string(), true);
+                }
+            }
+        }
+    });
+    submit.add_event_listener_with_callback("click", submit_callback.as_ref().unchecked_ref())?;
+    submit_callback.forget();
+
+    // output.set_inner_html("Hello from Rust!");
+
+    Ok(())
+}
